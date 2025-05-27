c9128d0e9eeccfe90110c1220a4efcd89fcaf9d7
Masab Ejaz <masabejaz@icloud.com>
2025-05-27 13:09:18 +0500

    version 9

--- src/components/appointment-chart.tsx
+++ src/components/appointment-chart.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 
@@ -9,6 +11,7 @@ export const AppointmentCalendar = () => {
     { title: 'Meeting', date: '2025-05-28' },
     { title: 'Dentist Appointment', date: '2025-05-29' },
     { title: 'Project Deadline', date: '2025-06-01' },
+    { title: 'Workshop', date: '2025-06-05' },
   ];
 
   return (
@@ -18,12 +21,24 @@ export const AppointmentCalendar = () => {
       </CardHeader>
       <CardContent>
         <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]}
+          plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
           initialView="dayGridMonth"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth',
+          }}
+          views={{
+            dayGridMonth: { buttonText: 'Month' },
+            timeGridWeek: { buttonText: 'Week' },
+            timeGridDay: { buttonText: 'Day' },
+            listMonth: { buttonText: 'List' },
+          }}
           events={events}
           selectable={true}
           dateClick={(info) => alert(`Clicked on ${info.dateStr}`)}
           eventClick={(info) => alert(`Event: ${info.event.title}`)}
+          height="auto"
         />
       </CardContent>
     </Card>
